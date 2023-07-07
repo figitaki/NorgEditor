2eca036882ebfa97bee97cdab8620e1a23a35ccd
Carey Janecka <figitaki96@yahoo.com>
2023-07-07 16:40:35 -0700

    Initial Commit

---
+++ NorgEditor/NorgEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorgEditorApp.swift
+//  NorgEditor
+//
+//  Created by Carey Janecka on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NorgEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
